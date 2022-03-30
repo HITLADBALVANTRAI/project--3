57cdf1e65d1361a201ae30d703b6785e71b1bcea
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-03-30 13:18:45 +0530

    Add files via upload

---
+++ project 3.py
@@ -0,0 +1,18 @@
+import turtle
+
+length = 10
+
+while length <= 150:
+    for i in range(0,10):
+        turtle.pencolor("red")
+        turtle.forward(length)
+        turtle.right(89)
+        length += 0.5
+        break
+    turtle.pencolor("green")
+    turtle.forward(length)
+    turtle.right(89)
+    length += 0.5
+
+
+
